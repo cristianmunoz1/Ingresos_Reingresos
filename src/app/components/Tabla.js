52b023d5843c7a1e3778114270e5d5759580bfd4
Cristian Muñoz <cristian.munoz3@udea.edu.co>
2023-05-29 23:57:18 -0500

    Se identan los archivos con eslint

--- src/app/components/Tabla.js
+++ src/app/components/Tabla.js
@@ -4,58 +4,65 @@ import ModalIngresoPuntaje from './Modal';
 import { useState } from 'react';
 import applicants from '../shared/data/applicants';
 
-
 const Tabla = () => {
-  
-  const [ search, setSearch ] = useState('');
-  
+  const [search, setSearch] = useState('');
 
   const searcher = (e) => {
     setSearch(e.target.value);
   };
 
   let results = [];
-  if(!search){
+  if (!search) {
     results = applicants;
-  }else{
-    results = applicants.filter((dato) => 
-    dato.id.toString().toLowerCase().includes(search.toLocaleLowerCase()));
+  } else {
+    results = applicants.filter((dato) =>
+      dato.id.toString().toLowerCase().includes(search.toLocaleLowerCase())
+    );
   }
 
   return (
-  <div className='contenedor-general'>
-    <div className='contenedor-input'>
-      <input value={search} onChange={searcher} type="text" placeholder='Número de identificación' className='form-control' />
-    </div>
-    
-    <div className="contenedor-tabla">
-      
-      <table className="bordered">
-        <thead>
-          <tr>
-            <th>Identificación</th>
-            <th>Nombre</th>
-            <th>Código de programa</th>
-            <th>Programa</th>
-            <th>Sede</th>
-            <th>Modalidad</th>
-          </tr>
-        </thead>
-        <tbody>
-          { results.map((users) => (
-            <tr key={users.id
-            }>
-              <td><ModalIngresoPuntaje text={users.id} name = {users.name}></ModalIngresoPuntaje></td>
-              <td>{users.name}</td>
-              <td>{users.programa.idPrograma}</td>
-              <td>{users.programa.programName}</td>
-              <td>{users.sede}</td>
-              <td>{users.modalidad}</td>
+    <div className="contenedor-general">
+      <div className="contenedor-input">
+        <input
+          value={search}
+          onChange={searcher}
+          type="text"
+          placeholder="Número de identificación"
+          className="form-control"
+        />
+      </div>
+
+      <div className="contenedor-tabla">
+        <table className="bordered">
+          <thead>
+            <tr>
+              <th>Identificación</th>
+              <th>Nombre</th>
+              <th>Código de programa</th>
+              <th>Programa</th>
+              <th>Sede</th>
+              <th>Modalidad</th>
             </tr>
-          )) }
-        </tbody>
-      </table>
-    </div>
+          </thead>
+          <tbody>
+            {results.map((users) => (
+              <tr key={users.id}>
+                <td>
+                  <ModalIngresoPuntaje
+                    text={users.id}
+                    name={users.name}
+                  ></ModalIngresoPuntaje>
+                </td>
+                <td>{users.name}</td>
+                <td>{users.programa.idPrograma}</td>
+                <td>{users.programa.programName}</td>
+                <td>{users.sede}</td>
+                <td>{users.modalidad}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
